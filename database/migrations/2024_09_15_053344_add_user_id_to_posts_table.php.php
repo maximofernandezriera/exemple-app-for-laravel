051b5db364ff025273fd8f2c6e5d058f3722b096
Máximo Fernández Riera <maxfernandez@gmail.com>
2024-09-16 06:01:31 +0200

    Database seed and factories.

---
+++ database/migrations/2024_09_15_053344_add_user_id_to_posts_table.php.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Add user_id column to posts table
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Drop user_id column from posts table
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+//        $category = new Category();
+//        $category->title = "Noves tecnologies";
+//        $category->url_clean = "noves_tecnologies";
+//        $category->start_date = "2024-09-15";
+//        $category->end_date = "2024-09-15";
+//        $category->save();
+
+        $this->call(CategorySeeder::class);
+
+        $AdminUser = new User();
+        $AdminUser->name = "Maximo";
+        $AdminUser->email = "maxfernandez@gmail.com";
+        $AdminUser->role = "superjefetop";
+        $AdminUser->password = Hash::make('12345678');
+        $AdminUser->save();
+
+        User::factory(5)->create();
+        Post::factory(5)->create();
+
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
